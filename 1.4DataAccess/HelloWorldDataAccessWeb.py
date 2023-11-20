cac56d36b58a407de797460bab132b0c52f38ba1
jhbrito <jbrito@ipca.pt>
2023-11-20 21:21:46 +0000

    small fix

--- 1.4DataAccess/HelloWorldDataAccessWeb.py
+++ 1.4DataAccess/HelloWorldDataAccessWeb.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 folder = "../Files"
 if not os.path.isfile(os.path.join(folder, "STOP_sign.jpg")):
     urllib.urlretrieve("https://upload.wikimedia.org/wikipedia/commons/f/f9/STOP_sign.jpg",
-                   os.path.join(folder, "STOP_sign.jpg"))
+                       os.path.join(folder, "STOP_sign.jpg"))
 image = PImage.open(os.path.join(folder, "STOP_sign.jpg"))
 plt.imshow(image, cmap=plt.cm.gray)
 plt.show()
